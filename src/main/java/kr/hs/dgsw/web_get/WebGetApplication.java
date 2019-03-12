f831535c3e7a0aea7ebb9c1c4fa35303aa03c820
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2019-03-12 17:27:29 +0900

    03.12 웹 숙제

--- src/main/java/kr/hs/dgsw/web_get/WebGetApplication.java
+++ src/main/java/kr/hs/dgsw/web_get/WebGetApplication.java
@@ -1,13 +1,13 @@
-package kr.hs.dgsw.calc;
+package kr.hs.dgsw.web_get;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalcApplication {
+public class WebGetApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalcApplication.class, args);
+        SpringApplication.run(WebGetApplication.class, args);
     }
 
 }
